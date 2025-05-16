426427ac8942d283b7c346664eed956026a915e1
Gayatri Shinde <130955861+Gayatrishinde21@users.noreply.github.com>
2025-05-16 09:22:41 +0530

    Create linux.py

---
+++ linux.py
@@ -0,0 +1,3 @@
+https://www.geeksforgeeks.org/linux-command-in-devops/
+https://www.tutorialworks.com/linux-for-devops/
+https://docs.google.com/document/u/0/d/1p7qYGDEtLrJDcsIuP--7vzafpiCvWlM_Xjk3RtvHBFQ/mobilebasic?fbclid=PAQ0xDSwKTlFBleHRuA2FlbQIxMAABp5JVsA6Mum-UPxyHHT3IguShqwnnaG2az67ENg1fvNP8bWSrw_HAuNwS7_1S_aem_5lMzgSg93Zc1nYa-wzvQUw
