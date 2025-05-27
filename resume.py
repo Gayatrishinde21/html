df3cef5751a98cbad5c56cd493f8a03e87c7bcca
Gayatri Shinde <130955861+Gayatrishinde21@users.noreply.github.com>
2025-05-27 12:47:28 +0530

    resume.py

---
+++ resume.py
@@ -0,0 +1,63 @@
+ASHWIN
+PRANAMI
+EDUCATION
+9824796261
+arpranami321@gmail.com
+Navrangpura, Ahmedabad -380009
+PROFILE INFO
+2010Intern as a Project Trainee at Amar InfoTech PVT.
+S.S.C
+Taxshila VidhyalayaLTD.. I am looking for opportunity that to be a part
+with 80%make a significant contribution to success of
+2012
+H.S.C
+Taxshila Vidhyalaya
+of Organization where I can utilize my skills and
+organization and help me to explore my knowledge
+and realize my potential .
+with 56%CERTIFICATIONS & PROJECTS
+20162016
+Central Medical Hub
+(B.E)
+In my college, I made PHP
+Modasa
+website as a project in team
+Bachelor of Engineering(I.T)
+Tatva Institute of Technological
+studies Modasa
+under the guidance of PROF.
+with CGPA : 6.56
+Hardik Soni
+Intern as a Project Trainee
+MY SKI LLS
+Professional Skills
+Python
+2022
+Amar Info.
+Ahmedabad
+city
+During my internship I made
+different types of web page, My
+portfolio, Python Games and I
+also made 'My Assistant'
+project represent my self much
+Frame Work-DJango
+better.
+Java Script
+Database : MySQL
+HTML
+CSS
+Personal Skills
+Leadership
+Personal Info
+Gender : Male
+Date Of Birth: 06/03/1995
+Address : 6/Laxmi Appt, Near. St. Kabir
+School, Navrangpura,
+Ahmedabad-380009
+Team Work
+Integrity
+Loyalty
+LanguagesEnglish
+KnownHindi
+Gujrati
